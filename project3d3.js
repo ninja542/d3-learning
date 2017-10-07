5bf078faf5c909e83a5dbd486d8f1d4e03821eac
ninja542 <theroboticcyborg@gmail.com>
2017-10-06 23:14:13 -0400

    axis and changing coordinates

--- project3d3.js
+++ project3d3.js
@@ -1,7 +1,7 @@
 var jsonRectangles = [
-{ "x_axis": 10, "y_axis": 10, "height": 20, "width":20, "color" : "green" },
-{ "x_axis": 160, "y_axis": 40, "height": 20, "width":20, "color" : "purple" },
-{ "x_axis": 70, "y_axis": 70, "height": 20, "width":20, "color" : "red" }];
+{ "x_axis": 10, "y_axis": 30, "height": 20, "width":20, "color" : "green" },
+{ "x_axis": 160, "y_axis": 60, "height": 20, "width":20, "color" : "purple" },
+{ "x_axis": 70, "y_axis": 90, "height": 20, "width":20, "color" : "red" }];
 var max_x = 0;
 var max_y = 0;
 
@@ -16,7 +16,7 @@ for(var i = 0; i<jsonRectangles.length; i++){
 		max_y = temp_y;
 	}
 }
-var svg_container = d3.select("body").append("svg").attr("width", max_x+20).attr("height", max_y+20);
+var svg_container = d3.select("body").append("svg").attr("width", max_x+20+30).attr("height", max_y+20).attr("transform", "translate(30,30)");
 var rectangles = svg_container.selectAll("rect")
 	.data(jsonRectangles)
 	.enter()
@@ -26,3 +26,10 @@ var rectangleAttributes = rectangles.attr("x", function(d){return d.x_axis;})
 	.attr("height", function(d){return d.height;})
 	.attr("width", function(d){return d.width;})
 	.style("fill", function(d){return d.color;});
+var xAxisScale = d3.scaleLinear().domain([0, max_x]).range([0, max_x]);
+var xAxis = d3.axisBottom(xAxisScale);
+var xAxisGroup = svg_container.append("g").call(xAxis);
+var yAxisScale = d3.scaleLinear().domain([0, max_y]).range([0, max_y]);
+var yAxis = d3.axisRight(yAxisScale);
+var yAxisGroup = svg_container.append("g").call(yAxis);
+// yAxisGroup.attr("transform", "rotate(90)");
