764623dfbb3f270ee6f25ec3d3dd7b62b32e0de6
ninja542 <theroboticcyborg@gmail.com>
2017-10-09 00:53:22 -0400

    mounted function. works

--- projectvued3.js
+++ projectvued3.js
@@ -1,13 +1,17 @@
 window.onload=function(){
 	var vm = new Vue({
+		el: "#app1",
 		data: {
 			radius: 5
 		},
-		computed: {
-			drawcircle: function(){
-				d3.select("#svgcontainer").selectAll("circle").data(this.radius).enter().append("circle").attr("r", function(d){return d;}).style("fill", "green");
-			}
+		mounted: function(){
+			var svg_container = d3.select("#app1").append("svg").attr("width", 500).attr("height", 500);
+			svg_container.selectAll("circle").data(this.radius).enter().append("circle").attr("r", 20).style("fill", "green");
 		},
+		// methods: {
+		// 	drawcircle: function(){
+		// 		d3.select(svg_container).selectAll("circle").data(this.radius).enter().append("circle").attr("r", function(d){return d;}).style("fill", "green");
+		// 	}
+		// },
 	});
-	vm.$mount("#app1");
 };
