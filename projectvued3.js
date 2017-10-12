9b7adadf9abdc642d7f738d04075fbba3a32b71e
ninja542 <theroboticcyborg@gmail.com>
2017-10-12 11:14:03 -0400

    finally worked!!!!

--- projectvued3.js
+++ projectvued3.js
@@ -2,16 +2,21 @@ window.onload=function(){
 	var vm = new Vue({
 		el: "#app1",
 		data: {
-			radius: 5
+			radius: [5]
 		},
 		mounted: function(){
 			var svg_container = d3.select("#app1").append("svg").attr("width", 500).attr("height", 500);
-			svg_container.selectAll("circle").data(this.radius).enter().append("circle").attr("r", 20).style("fill", "green");
+			console.log(this.radius);
+			circle = svg_container.selectAll("circle").data(this.radius).enter().append("circle")
+				.attr("r", function(d){return d})
+				.attr("cx", 250)
+				.attr("cy", 250)
+				.style("fill", "green");
 		},
-		// methods: {
-		// 	drawcircle: function(){
-		// 		d3.select(svg_container).selectAll("circle").data(this.radius).enter().append("circle").attr("r", function(d){return d;}).style("fill", "green");
-		// 	}
-		// },
+		watch: {
+			radius: function(newValue){
+				circle.attr("r", this.radius);
+			}
+		},
 	});
 };
