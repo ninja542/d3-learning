6aebe0696d43745afd15bb9faa0dde8d919aba9c
ninja542 <18501292+ninja542@users.noreply.github.com>
2018-09-06 21:33:45 -0400

    clean  up stuff for linting, blue dot begone

--- projectvued3.js
+++ projectvued3.js
@@ -8,7 +8,7 @@ window.onload=function(){
 			var svg_container = d3.select("#app1").append("svg").attr("width", 500).attr("height", 500);
 			console.log(this.radius);
 			circle = svg_container.selectAll("circle").data(this.radius).enter().append("circle")
-				.attr("r", function(d){return d})
+				.attr("r", function(d){return d;})
 				.attr("cx", 250)
 				.attr("cy", 250)
 				.style("fill", "green");
